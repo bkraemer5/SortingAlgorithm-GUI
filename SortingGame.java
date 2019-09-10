0a8de1fd98905dac47e07467d59202371a8d4428
Brittany Kraemer <brittanykraemer@macbook-pro.home>
2019-09-09 22:18:17 -0700

    Added thread and additional methods to SortingGame class

--- SortingGame.java
+++ SortingGame.java
@@ -10,6 +10,9 @@ public class SortingGame extends Canvas {
 	Window window;
 	public static final float WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
 	
+	private Thread thread;
+	private boolean running = false;
+	
 	public enum STATE {
 		Title,
 		MainFrame,
@@ -23,8 +26,62 @@ public class SortingGame extends Canvas {
 		new Window(WIDTH, HEIGHT, "Sorting", this);
 	}
 	
+	public synchronized void stop() {
+		try {
+			// .join() stops the thread
+			thread.join();
+			running = false;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized void start() {
+		thread = new Thread();
+		thread.start();
+		running = true;
+	}
+	
+	public void run() {
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running) {
+				render();
+			}
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		
+	}
+	
 	public static void main(String args[]) { 
-		SortingGame start = new SortingGame();
+		new SortingGame();
 	}
 	
 	
